1fb330b40c371dee760d8d36ca4b936427541285
Tumelo <junkmailmooo@gmail.com>
2023-12-20 12:33:42 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,2 @@
-# welcome to 0x02 c function
-Thi is an introduction to loops and nested loops
+#This is an intro to nested loops
+welcome to nested loops
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/*
- * File: main.h
- *
- * Desc: Header file containing declartions for all function
- */
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -16,9 +10,9 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void time_teble(void);
+void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_time_table(int n);
+void print_times_table(int n);
 
-#endife
+#endif
